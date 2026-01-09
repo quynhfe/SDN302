82b652c2ea6ddee44c23307d9a79e400d1280fb2
Tong Thi Nhu Quynh <quynhfe99@gmail.com>
2026-01-09 15:46:13 +0700

    Demo http

---
+++ demo-htttp/index.js
@@ -0,0 +1,13 @@
+var http = require("http");
+const hostname = "localhost";
+const port = 3000;
+http
+  .createServer(function (req, res) {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World\n");
+  })
+  .listen(port);
+
+console.log(`Server running at http://${hostname}:${port}/`);
